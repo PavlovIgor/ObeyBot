613f3b7a3df620db33242b729b0c2951b1afe0ec
Igor Pavlov <igor@artlog.pro>
2018-03-06 15:26:43 +0300

    worked on prod

--- app/controllers/telegram_webhook_controller.rb
+++ app/controllers/telegram_webhook_controller.rb
@@ -8,19 +8,6 @@ class TelegramWebhookController < Telegram::Bot::UpdatesController
     respond_with :message, text: "Hello #{responce_appeal(from)}! Let me know little more about you." if from
   end
 
-  # Be sure to use splat args and default values
-  # to not get errors when someone passed
-  # more or less arguments in the message.
-  def help(cmd = nil, *)
-    message =
-      if cmd
-        help_for_cmd?(cmd) ? t(".cmd.#{cmd}") : t('.no_help')
-      else
-        t('.help')
-      end
-    reply_with text: message
-  end
-
 private
   def responce_appeal(from)
     if from.key?("first_name") and from.key?("last_name")
