e7919e4a5924c4e06ed202e8f2d739bd5512dfc5
Igor Pavlov <igor@artlog.pro>
2018-03-08 15:31:50 +0300

    buttons

--- app/controllers/telegram_webhook_controller.rb
+++ app/controllers/telegram_webhook_controller.rb
@@ -11,24 +11,26 @@ class TelegramWebhookController < Telegram::Bot::UpdatesController
     respond_with :message, text: ObeyBotFacade::start(from) if from
   end
 
-  context_handler :age_waiting do |*words|
-    if (Integer(words[0]) rescue false)
+  def age_waiting(age = nil, *)
+    if (Integer(age) rescue false)
       save_context :gender_waiting
-      respond_with :message, text: ObeyBotFacade::set_age(words), reply_markup: ObeyBot.gender_keyboard
+      respond_with :message, text: ObeyBotFacade::set_age(age), reply_markup: ObeyBot.gender_keyboard
     else
       save_context :age_waiting
       respond_with :message, text: "Неверный формат. Повторите попытку."
     end
   end
 
-  context_handler :gender_waiting do |*words|
-    respond_with :message, text: update
-    # if words[0] == "Муж" or words[0] == "Жен"
-    #   respond_with :message, text: ObeyBotFacade::set_gender(words[0]), reply_markup: ObeyBot.skills_keyboard
-    # else
-    #   save_context :gender_waiting
-    #   respond_with :message, text: "Неверный формат. Повторите попытку.", reply_markup: ObeyBot.gender_keyboard
-    # end
-  end
+
+  # context_handler :gender_waiting do |*words|
+  #   save_context :gender_waiting
+  #   respond_with :message, text: update
+  #   # if words[0] == "Муж" or words[0] == "Жен"
+  #   #   respond_with :message, text: ObeyBotFacade::set_gender(words[0]), reply_markup: ObeyBot.skills_keyboard
+  #   # else
+  #   #   save_context :gender_waiting
+  #   #   respond_with :message, text: "Неверный формат. Повторите попытку.", reply_markup: ObeyBot.gender_keyboard
+  #   # end
+  # end
 
 end
