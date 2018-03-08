1eb707a5581969e59c3596cae0785a89cc3ef58b
Igor Pavlov <igor@artlog.pro>
2018-03-08 16:17:50 +0300

    buttons

--- app/controllers/telegram_webhook_controller.rb
+++ app/controllers/telegram_webhook_controller.rb
@@ -1,7 +1,6 @@
 class TelegramWebhookController < Telegram::Bot::UpdatesController
-  skip_before_action :verify_authenticity_token
   include Telegram::Bot::UpdatesController::MessageContext
-  context_to_action!
+
 
   def message(message)
     respond_with :message, text: 'message'
