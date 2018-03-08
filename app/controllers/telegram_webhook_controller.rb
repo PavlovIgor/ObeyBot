e76b7a334a3fd20d5e7d7dd1e565c67ccfea3afb
Igor Pavlov <igor@artlog.pro>
2018-03-08 23:12:13 +0300

    buttons

--- app/controllers/telegram_webhook_controller.rb
+++ app/controllers/telegram_webhook_controller.rb
@@ -23,7 +23,7 @@ class TelegramWebhookController < Telegram::Bot::UpdatesController
 
   def callback_query(data)
     if data == 'Муж' or data == "Жен"
-      answer_callback_query 'cool'
+      respond_with :message, text: ObeyBotFacade::set_gender(gender), reply_markup: ObeyBot.skills_keyboard
     else
       answer_callback_query 'er'
     end
