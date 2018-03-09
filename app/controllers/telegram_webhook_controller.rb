226f1bfba6bddc43dd5776f92a8c7e536327e633
Igor Pavlov <igor@artlog.pro>
2018-03-09 16:30:51 +0300

    remove keyboard

--- app/controllers/telegram_webhook_controller.rb
+++ app/controllers/telegram_webhook_controller.rb
@@ -47,7 +47,7 @@ class TelegramWebhookController < Telegram::Bot::UpdatesController
   def skill_level(data = nil, *)
     data.downcase!
     if [ObeyBot.vars[:low_skill_level_var], ObeyBot.vars[:medium_skill_level_var], ObeyBot.vars[:high_skill_level_var]].include? data
-      respond_with :message, text: ObeyBotFacade.skill_level_answer(data, update["message"]['from']['id'])
+      respond_with :message, text: ObeyBotFacade.skill_level_answer(data, update["message"]['from']['id']), reply_markup: ObeyBot.remove_keyboard
       respond_with :message, text: ObeyBotFacade.user_program(update["message"]['from']['id']), reply_markup: ObeyBot.skills_keyboard
     else
       save_context :skill_level
